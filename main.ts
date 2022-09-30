465fa7ffcd9ff5249e612dafa60e060dc0f39676
Siddharth Singh <simsid66@gmail.com>
2022-09-30 17:02:55 +0530

    version cryptojs properly

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import * as path from "https://deno.land/std@0.156.0/path/mod.ts";
 import * as fs from "https://deno.land/std@0.156.0/fs/mod.ts";
 import dir from "https://deno.land/x/dir@1.5.1/mod.ts";
 import { getReasonPhrase } from "https://deno.land/x/https_status_codes@v1.2.0/mod.ts";
-import CryptoJS from "https://esm.sh/crypto-js";
+import CryptoJS from "https://esm.sh/crypto-js@4.1.1";
 
 type Rec = Record<string, any>;
 
