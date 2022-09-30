6b3cb71ef96c32f6b9030ac15263bb0c8782d043
Siddharth Singh <simsid66@gmail.com>
2022-09-30 20:34:21 +0530

    create deps.ts

---
+++ src/deps.ts
@@ -0,0 +1,7 @@
+export * as flags from "https://deno.land/std@0.156.0/flags/mod.ts";
+export * as path from "https://deno.land/std@0.156.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.156.0/fs/mod.ts";
+export { getReasonPhrase } from "https://deno.land/x/https_status_codes@v1.2.0/mod.ts";
+import CryptoJS from "https://esm.sh/crypto-js@4.1.1";
+import dir from "https://deno.land/x/dir@1.5.1/mod.ts";
+export { CryptoJS, dir };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
-import * as flags from "https://deno.land/std@0.156.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.156.0/path/mod.ts";
-import * as fs from "https://deno.land/std@0.156.0/fs/mod.ts";
-import dir from "https://deno.land/x/dir@1.5.1/mod.ts";
-import { getReasonPhrase } from "https://deno.land/x/https_status_codes@v1.2.0/mod.ts";
-import CryptoJS from "https://esm.sh/crypto-js@4.1.1";
+import { path, fs, flags, dir, CryptoJS, getReasonPhrase } from "./deps.ts";
 
 type Rec = Record<string, any>;
 
